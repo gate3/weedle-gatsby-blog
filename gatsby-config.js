9bd548ed3ad54c52b559713f172efd4ef2b9888a
Doyin Olarewaju <youngenterpreneur11@gmail.com>
2022-11-30 23:37:21 -0700

    Upgrade to gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@
 
 module.exports = {
   /* Your site config here */
-  siteMetadata: require("./site-meta-data.json"),
+  siteMetadata: require('./site-meta-data.json'),
   plugins: [
     {
       resolve: `gatsby-source-filesystem`,
@@ -18,28 +18,52 @@ module.exports = {
     {
       resolve: `gatsby-transformer-remark`,
       options: {
-        plugins: [{
-          resolve: `gatsby-remark-prismjs`,
-          options: {
-            classPrefix: "language-",
-            inlineCodeMarker: null,
-            aliases: {},
-            showLineNumbers: false,
-            noInlineHighlight: false,
+        plugins: [
+          {
+            resolve: `gatsby-remark-prismjs`,
+            options: {
+              classPrefix: 'language-',
+              inlineCodeMarker: null,
+              aliases: {},
+              showLineNumbers: false,
+              noInlineHighlight: false,
+            },
           },
-        },
-        {
-          resolve: 'gatsby-remark-emojis',
-        }],
+          {
+            resolve: 'gatsby-remark-emojis',
+          },
+        ],
       },
     },
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
         // The property ID; the tracking code won't be generated without it. replace with yours
-        trackingId: "UA-164743872-1",
+        trackingIds: [
+          'G-1Q5RFKCQCR', // Google Analytics / GA
+        ],
         head: true,
-      }
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        /* gtagConfig: {
+          optimize_id: 'OPT_CONTAINER_ID',
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ['/preview/**', '/do-not-track/me/too/'],
+          // Defaults to https://www.googletagmanager.com
+          origin: 'YOUR_SELF_HOSTED_ORIGIN',
+          // Delays processing pageview events on route update (in milliseconds)
+          delayOnRouteUpdate: 0, */
+        },
+      },
     },
     {
       resolve: `gatsby-plugin-manifest`,
@@ -50,10 +74,10 @@ module.exports = {
         background_color: `#fff`,
         theme_color: `#381696`,
         display: `standalone`,
-        icon: "src/images/icon.png",
+        icon: 'src/images/icon.png',
       },
     },
-    `gatsby-plugin-sass`, 
+    `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-netlify-cms`,
     'gatsby-plugin-dark-mode',
@@ -61,4 +85,4 @@ module.exports = {
     `gatsby-plugin-sitemap`,
     `gatsby-plugin-offline`,
   ],
-}
+};
